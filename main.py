5af3e961d52640fc3c597f6ff99becd5d88ed4df
hobee-stonewise <lzqpublic@163.com>
2023-05-24 12:35:17 +0800

    add welcome

---
+++ main.py
@@ -0,0 +1,30 @@
+# App Entry
+import argparse
+import os
+import sys
+
+from src import *
+# from src import app
+# from src import config
+# from src import logger
+# from src import utils
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Run the app')
+parser.add_argument('--config', type=str, default='config.yaml', help='Path to config file')
+
+
+def main():
+    args = parser.parse_args()
+    # config_path = args.config
+    # if not os.path.exists(config_path):
+    #     print("Config file not found: {}".format(config_path))
+    #     sys.exit(1)
+    # config.load_config(config_path)
+    # logger.init_logger(config.get_config().get('log'))
+    # utils.init_utils(config.get_config().get('utils'))
+    # app.run()
+
+if __name__ == "__main__":
+    main()
+
---
+++ src/__init__.py
@@ -0,0 +1,25 @@
+import os
+
+from .version import __version__, digital_version
+
+package_path = os.path.dirname(__file__)
+
+
+def _welcome():
+    print('+---------------------------------------+')
+    print('|      open Fund Reminder Toolkit       |')
+    print('|      ^^^^^^^^^^^^^^^^^^^^^^^^^^       |')
+    print('|          author: Hobee Liu            |')
+    print('+--------------------------------------+')
+    print(
+        'NOTICE: This software is licensed under the GNU General Public\n'
+        'License v3.0 and SHALL NOT be used for commercial purpose. All\n'
+        'rights reserved to HobeeLiu. Please contact me if the project\n'
+        'has any potential infringement risks.',
+    )
+
+
+_welcome()
+
+
+__all__ = ['__version__', 'digital_version', 'package_path']
